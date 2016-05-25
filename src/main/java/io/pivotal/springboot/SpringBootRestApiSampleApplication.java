c43faf4b095367a5d9c67372aacec49592640d47
Lei Xu <lxu@pivotal.io>
2016-05-25 16:01:58 +0900

    init

---
+++ src/main/java/io/pivotal/springboot/SpringBootRestApiSampleApplication.java
@@ -0,0 +1,12 @@
+package io.pivotal.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApiSampleApplication.class, args);
+	}
+}
