7cef2ff6def57d245f9f5a29e9e70c35ad349ec7
Lei Xu <lxu@pivotal.io>
2016-05-25 16:31:39 +0900

    sample rest api implemented based on spring boot tutorial

---
+++ src/main/java/io/pivotal/springboot/web/GreetingController.java
@@ -0,0 +1,22 @@
+package io.pivotal.springboot.web;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.*;
+import io.pivotal.springboot.domain.*;
+
+
+/**
+ * Created by lei_xu on 5/25/16.
+ */
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+}
